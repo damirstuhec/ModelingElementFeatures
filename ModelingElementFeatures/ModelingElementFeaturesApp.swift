d5ab8f73d870efd1e5c2db3df7834a3c4cd98602
Damir Stuhec <damir.stuhec@gmail.com>
2020-09-01 14:10:18 +0200

    Initial Commit

---
+++ ModelingElementFeatures/ModelingElementFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelingElementFeaturesApp.swift
+//  ModelingElementFeatures
+//
+//  Created by Damir Stuhec on 01/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ModelingElementFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
